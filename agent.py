023feac268906a5ec717b06f1499df7d57698376
jennalee <yunjung.jenna.lee@gmail.com>
2020-03-16 12:42:12 -0700

    vectorized

--- agent.py
+++ agent.py
@@ -27,16 +27,16 @@ class Robot():
         self.control=[]
         self.u  = np.copy(self.u0)
         self.u_prev = np.copy(self.u0)
-        self.lambd= np.zeros(((self.H),self.K))
+        self.lambd= np.zeros_like(self.u0)
         self.rho = rho
         self.init_M()
 
         self.neighbors_dict={}
         self.W = np.zeros((self.dim,(self.H-1)*self.dim))
         self.W = np.hstack((self.W,np.eye(2))) #x' = Wx propagation matrix
+        self.W =np.kron(np.eye(self.K,dtype=int),self.W)
         # pdb.set_trace()
         self.method = method
-        self.const=2
 
     def init_M(self):
         M = np.zeros((self.H*self.dim,(self.H)))
@@ -50,10 +50,11 @@ class Robot():
         col[0:2,0:2]=np.eye(2)
         for i in range(self.H):
             col[self.dim*(i+1):self.dim*(i+2),:] = self.A @ col[self.dim*(i):self.dim*(i+1),:]
-
-        # self.M = np.hstack((col,np.vstack((np.zeros((self.dim,M.shape[1])),M))))
-        self.M = M
-        self.col = col[2:,:] # propagation of how each robot propagates its init position thru time
+        # self.M = M
+        self.M = np.kron(np.eye(self.K,dtype=int),M)
+        # pdb.set_trace()
+        # self.col = col[2:,:].reshape(-1,1) # propagation of how each robot propagates its init position thru time
+        self.col = np.kron(np.eye(self.K),col[2:,:])
 
     def get_neighbors(self):
         # return np.mod([self.index-1+self.K,self.index+1],self.K)
@@ -64,20 +65,27 @@ class Robot():
         return self.u
 
     def augmented_lagrangian(self,u):
-        u=u.reshape(-1,self.K)
+        # u=u.reshape(-1,self.K)
+        # u = np.expand_dims(u,axis=1)
+        u=u.reshape(-1,1)
         # pdb.set_trace()
         reach_goal =self.W @(self.col @ self.inits + self.M @ u )-self.goals
         regularization = 0
         cost = 0.5 * np.linalg.norm(reach_goal,2) ** 2
-        self.away_from_the_goal=(cost*2)/100
+        self.away_from_the_goal=(cost*2)
+
 
         for j in range(self.K):
             if j == self.index:
                 pass #myself, skip
             else:
                 uj_prev = self.neighbors_dict[j]
-                regularization += np.linalg.norm(u-(self.u_prev+uj_prev)/2)**2
-        cost += self.rho/2.0 * regularization + self.const
+                regularization += np.linalg.norm(u-(self.u_prev+uj_prev)/2 )**2
+        # pdb.set_trace()
+        self.regularization=regularization
+        cost += self.rho/2.0 * regularization +(u.T @ self.lambd)[0,0]
+
+        # pdb.set_trace()
         '''
         todo: add collision avoidance
         first two values of u should not change over iteration
@@ -90,9 +98,12 @@ class Robot():
                                     method=self.method)#,
         self.cost=result['fun']
         # print('cost: %.3f'%self.cost)
-        return result['x'].reshape(-1,self.K)
+        # pdb.set_trace()
+        # return result['x'].reshape(-1,self.K)
+        return np.expand_dims(result['x'],axis=1)
 
     def dual_update(self, new_u):
+        # pdb.set_trace()
         new_lambd=self.lambd
         for j in self.neighbors_dict.keys():
             new_lambd +=  self.rho*( new_u- self.neighbors_dict[j])
--- run.py
+++ run.py
@@ -6,28 +6,18 @@ import matplotlib.pyplot as plt
 from run_admm import ADMMSolver
 #
 A =  np.array([[.2, .5],[.3, .4]])
-B  = np.array([[1],[1]])
+B  = np.array([[.4],[.6]])
+def simulate_the_rest_and_plot(X,U,all_u,inits,goals,iter):
+    dim = 2
+    K = int(inits.shape[0]/dim)
+    for i in range(1,all_u.shape[0]):
+        new_u=all_u[[i],:]
+        U=np.vstack((U,new_u))
+        # pdb.set_trace()
+        new_pos=A@X[-1*dim:] + B@new_u
+        X = np.vstack((X,new_pos))
+    make_plots(X,inits.reshape(-1,K),goals.reshape(-1,K),iter)
 
-# def simulate(robots, xprev,u):
-#
-#     # K = len(robots)
-#     # T = robots[0].u.shape[0]
-#     # dim = start.shape[0]
-#     # x = np.zeros((dim*(T+1),K))
-#
-#     #since ADMM converged, assumption is that the solution U is the same!
-#     pdb.set_trace()
-#     x = A@xprev
-#
-#     # for k in range(K):
-#     #     robot = robots[k]
-#     #     robot.control.append(robot.u[0])
-#     #     x[:2,[k]] = start[:,[k]]
-#     #     for t in range(1,T+1):
-#     #         x[dim*t:dim*(t+1),[k]] = robot.A @ x[dim*(t-1):dim*(t),[k]] + \
-#     #                         robot.B @ np.expand_dims(robot.u[t-1],axis=1)
-#     # pdb.set_trace()
-#     return A@xprev + B@u
 
 def process_x(x):
     M,N = x.shape
@@ -39,20 +29,31 @@ def process_x(x):
     # pdb.set_trace()
     return xx, xy
 
-def make_plots(x,goals,iter):
+def make_plots(X,inits,goals,iter):
     plt.figure()
-    T,K = x.shape
-    time = np.linspace(0,T-1,T)
+    # pdb.set_trace()
+    T,K = X.shape
+    # time = np.linspace(0,T-1,T)
     filename = './results/traj_iter'+str(iter)+'.png'
-    x,y = process_x(x)
+    x,y = process_x(X)
+    # cmap = get_cmap(K*2)
     for k in range(K):
         lbl ='robot'+str(k)
+        goal_lbl = lbl + ' goal'
+        start_lbl = lbl + ' start'
         # pdb.set_trace()
-        plt.plot(x[:,k],y[:,k],label=lbl,alpha=0.5)
-    plt.legend()
-    # plt.saveas(filename)
+        # color=cmap(k)
+        cmap = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
+        plt.plot(x[:,k],y[:,k],label=lbl,c=cmap[k],alpha=0.5)
+        # pdb.set_trace()
+        plt.scatter(inits[0,k],inits[1,k],c=cmap[k],marker='o',label=start_lbl)
+        plt.scatter(goals[0,k],goals[1,k],c=cmap[k],marker='*',label=goal_lbl)
+
+    plt.legend()
+    plt.savefig(filename)
     # plt.show()
+    # print('go big and go home')
 
 
 def simulate_and_plot(robots,inits, goals,iter):
@@ -86,13 +87,15 @@ def main():
 
 
     # inits = np.array([[1,0],[0,2],[4,4],[2,0]]).T
-    # goals = np.array([[4,4],[2,0],[0,0],[1,1]]).T
-    inits = np.array([[1,0],[0,2]]).T
-    goals = np.array([[4,4],[2,0]]).T
+    # goals = np.array([[4,3],[1,2],[0,0],[1,1]]).T
+    inits = np.array([[1],[0],[0],[2]])
+    goals = np.array([[4],[4],[2],[0]])
     # col,M_part = build_M(H=args.horizon, dim=args.dim,K=args.num_agents)
 
     np.set_printoptions(precision=3,suppress=True)
-    random_u0=np.float64(np.random.randint(-2,5,size=(args.horizon,args.num_agents)))
+    random_u0=np.float64(np.random.randint(-2,5,size=(args.horizon*args.num_agents,1)))
+    # pdb.set_trace()
+    # random_u0
     robots= []
     for i in range(args.num_agents):
         robots.append(Robot(A = A,
@@ -120,49 +123,47 @@ def main():
     track_results =np.zeros((1,args.num_agents))
     np.set_printoptions(precision=2, suppress=True)
     U = np.ones((1,args.num_agents))
-    X = inits
+    X = inits.reshape(-1,args.num_agents)
     while True:
         admm.solve()
         if np.mod(traj_count,10) ==0 and traj_count>0:
             print('iter %d .... ' %traj_count)
-            pdb.set_trace()
-        new_u=robots[0].u[[0],:]
+        new_u=robots[0].u.reshape(-1,args.num_agents)[[0],:]
         U=np.vstack((U,new_u))
+        # pdb.set_trace()
         # A@xprev + B@u
         new_pos=A@X[-args.dim:] + B@new_u
         # simulate(robots,,new_u)
         X = np.vstack((X,new_pos))
-        #update u0 values
+        simulate_the_rest_and_plot(X,U,robots[0].u.reshape(-1,args.num_agents),
+                                    inits,goals,traj_count)
+
+        #update u0 & x0 values
         for k in range(args.num_agents):
             robots[k].u0=np.copy(robots[0].u)
-            robots[k].inits=np.copy(new_pos)
+            # robots[k].inits=np.copy(new_pos)
         # paths=np.vstack((paths,next_pos))
         # pdb.set_trace()
-        how_close=np.linalg.norm(new_pos-goals)
-        print(how_close)
+        # print(goals)
+        # print(new_pos)
+        how_close=np.linalg.norm(new_pos-goals.reshape(-1,args.num_agents))
+        # print(how_close)
 
-        if np.linalg.norm(new_pos-goals)<1:
+        if np.linalg.norm(new_pos-goals.reshape(-1,args.num_agents))<1:
             print('reached the goal')
             break
 
-        #update starting positions for all robots
-        # for k in range(args.num_agents):
-        #     robot = robots[k]
-        #     # pdb.set_trace()
-        #     robot.x0 = next_pos[:,[k]]
-        #
-        #     track_results[0,k] = int(np.linalg.norm(robot.x0 - goals[:,[k]],2) < 0.1)
-        #
-        # if np.sum(track_results) >=2:
-        #     print('success lol')
-        #     break
         if traj_count >=args.max_steps:
             print('traj failed to converge, loop broken by the safety counter')
+            # simulate the rest and go
+
             break
         traj_count +=1
 
-    pdb.set_trace()
-    make_plots(paths,goals,iter)
+    # pdb.set_trace()
+    iter ='FINAL'
+    make_plots(X,inits.reshape(-1,args.num_agents),goals.reshape(-1,args.num_agents),iter)
+    # pdb.set_trace()
     print('')
 
 
